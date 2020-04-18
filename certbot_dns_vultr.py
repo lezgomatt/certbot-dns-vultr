d6d220f607972193eaff73431fe9b8d13357afd8
Matt <undecidabot@gmail.com>
2020-04-18 19:23:59 +0800

    cleanup: Various improvements especially with error handling

--- certbot_dns_vultr.py
+++ certbot_dns_vultr.py
@@ -52,67 +52,45 @@ class Authenticator(dns_common.DNSAuthenticator):
         return self.vultr
 
 
-class VultrClient(object):
+class VultrClient:
     def __init__(self, key):
         self.api_key = key
 
     def add_txt_record(self, domain_name, record_name, record_data):
         try:
             base_domain_name = self.get_base_domain_name(domain_name)
-        except Error as err:
-            raise errors.PluginError(f'Failed to add TXT record for "{domain_name}": {err}')
+            record_name = self.get_relative_record_name(base_domain_name, record_name)
 
-        record_name = self.get_relative_record_name(base_domain_name, record_name)
-
-        try:
             self.request("POST", "/dns/create_record", {
                 "domain": base_domain_name,
                 "type": "TXT",
                 "name": record_name,
                 "data": quote(record_data),
             })
-        except requests.HTTPError as err:
-            raise errors.PluginError(f'Error adding TXT record "{record_name}" for "{base_domain_name}": {response_error_message(err.response)}')
 
-        logger.debug(f'Successfully added TXT record "{record_name}" for "{base_domain_name}"')
+            logger.debug(f'Successfully added TXT record "{domain_name}"')
+        except (VultrPluginError, requests.HTTPError) as err:
+            error_message = err.message if isinstance(err, VultrPluginError) else response_error_message(err)
+            raise errors.PluginError(f'Failed to add TXT record for "{domain_name}": {error_message}')
 
     def del_txt_record(self, domain_name, record_name, record_data):
         try:
             base_domain_name = self.get_base_domain_name(domain_name)
-        except Error as err:
-            logger.warning(f'Failed to delete TXT record for "{domain_name}": {err}')
-            return
+            record_name = self.get_relative_record_name(base_domain_name, record_name)
+            record_id = self.get_record_id(base_domain_name, record_name, record_data)
 
-        record_name = self.get_relative_record_name(base_domain_name, record_name)
-
-        try:
-            dns_records = self.request("GET", "/dns/records?domain=" + base_domain_name)
-        except requests.HTTPError as err:
-            error_message = f'Error fetching DNS records for "{base_domain_name}": {response_error_message(err.response)}'
-            logger.warning(f'Failed to delete TXT record for "{domain_name}": {error_message}')
-            return
-
-        for r in dns_records:
-            if r.get("type") == "TXT" and r.get("name") == record_name and r.get("data") == quote(record_data):
-                record_id = r["RECORDID"]
-                break
-        else:
-            logger.warning(f'Failed to delete TXT record "{record_name}" for "{base_domain_name}": TXT record not found')
-            return
-
-        try:
             self.request("POST", "/dns/delete_record", {"domain": base_domain_name, "RECORDID": record_id})
-        except requests.HTTPError as err:
-            logger.warning(f'Error deleting TXT record "{record_name}" for "{base_domain_name}": {response_error_message(err.response)}')
-            return
 
-        logger.debug(f'Successfully deleted TXT record "{record_name}" for "{base_domain_name}"')
+            logger.debug(f'Successfully deleted TXT record "{domain_name}"')
+        except (VultrPluginError, requests.HTTPError) as err:
+            error_message = err.message if isinstance(err, VultrPluginError) else http_error_message(err)
+            logger.warning(f'Failed to delete TXT record for "{domain_name}": {error_message}')
 
     def get_base_domain_name(self, full_domain_name):
         try:
             domains = self.request("GET", "/dns/list")
         except requests.HTTPError as err:
-            raise Error("Error fetching DNS domains list: " + response_error_message(err.response))
+            raise VultrPluginError("Error fetching DNS domains list: " + http_error_message(err))
 
         guess_list = dns_common.base_domain_name_guesses(full_domain_name)
         for guess in guess_list:
@@ -121,10 +99,22 @@ class VultrClient(object):
                     logger.debug(f'Using base domain "{guess}" for "{full_domain_name}"')
                     return guess
 
-        raise Error(f'Could not find the (base) domain for "{full_domain_name}" (Is the domain set in your DNS?)')
+        raise VultrPluginError(f'Could not find the (base) domain for "{full_domain_name}" (Is the domain set in your DNS?)')
 
     def get_relative_record_name(self, base_domain_name, absolute_record_name):
         return absolute_record_name[:-len("." + base_domain_name)]
+
+    def get_record_id(self, base_domain_name, relative_record_name, record_data):
+        try:
+            dns_records = self.request("GET", "/dns/records?domain=" + base_domain_name)
+        except requests.HTTPError as err:
+            raise VultrPluginError(f'Error fetching DNS records for "{base_domain_name}": {http_error_message(err)}')
+
+        for r in dns_records:
+            if r.get("type") == "TXT" and r.get("name") == relative_record_name and r.get("data") == quote(record_data):
+                return r["RECORDID"]
+
+        raise VultrPluginError("TXT record not found")
 
     def request(self, method, path, data=None):
         url = "https://api.vultr.com/v1" + path
@@ -138,11 +128,13 @@ class VultrClient(object):
             return response.text
 
 
-class Error(Exception):
-    pass
+class VultrPluginError(Exception):
+    def __init__(self, message):
+        self.message = message
 
 
-def response_error_message(response):
+def http_error_message(http_error):
+    response = http_error.response
     return f"{response.status_code} - {response.text}"
 
 def quote(text):
